1ab074e643402104b99c215813839cc1a4df2238
Your Name <you@example.com>
2023-04-15 00:09:45 -0700

    added an export

--- functions/index.js
+++ functions/index.js
@@ -28,3 +28,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// export const app = initializeApp(firebaseConfig);
